fe1b640e06eccd13b4f103f853e55fe4965549c1
alex <panagiotopoulos.alexandros@gmail.com>
2020-02-17 19:43:06 +0000

    added identifinding the CSS21 type color of a specific image pixel

--- colour_identification.py
+++ colour_identification.py
@@ -1,9 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import webcolors
+
+
+def get_closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.CSS21_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
 
 image = cv2.imread('images/test-sample-teal.png')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-print("The type of this input is {}".format(type(image)))
-print("Shape: {}".format(image.shape))
-print(image[1000,1000:1010])
+
+requested_colour = image[1000, 1000]
+print(get_closest_colour(requested_colour))
+
+# print("The type of this input is {}".format(type(image)))
+# print("Shape: {}".format(image.shape))
+# print(image[1000, 1000:1010])
